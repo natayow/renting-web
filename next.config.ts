434acd68b4fb811a3c581e40d223f1dc7ea56de6
natayow <nathasya1616@gmail.com>
2025-12-07 16:46:37 +0700

    sambungin frontend & backend untuk property list di landing page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["localhost", "images.unsplash.com", "example.com"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
